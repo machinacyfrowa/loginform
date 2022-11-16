0347f793cbf2ee5c14fa350b01bbab0737acb8d8
Pawel Zawadzki <admin@machinacyfrowa.pl>
2022-11-16 14:03:52 +0100

    implement user registration

--- class/User.class.php
+++ class/User.class.php
@@ -9,6 +9,18 @@ class User {
     public function __construct(string $login, string $password) {
         $this->login = $login;
         $this->password = $password;
+        $this->firstName = "";
+        $this->lastName = "";
+    }
+
+    public function register() {
+        $passwordHash = password_hash($this->password, PASSWORD_ARGON2I);
+        $query = "INSERT INTO user VALUES (NULL, ?, ?, ?, ?)";
+        $db = new mysqli('localhost', 'root', '', 'loginForm');
+        $preparedQuery = $db->prepare($query); 
+        $preparedQuery->bind_param('ssss', $this->login, $passwordHash, 
+                                            $this->firstName, $this->lastName);
+        $preparedQuery->execute();
     }
 }
 ?>
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once('class/User.class.php');
 
 $user = new User('jkowalski', 'tajneHasło');
+$user->register();
 
 echo '<pre>';
 var_dump($user);
