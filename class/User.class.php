fe3f9a4a9674a8978a4e35ddf71f306e7d6121d0
Pawel Zawadzki <admin@machinacyfrowa.pl>
2022-11-16 14:25:39 +0100

    login implemented

--- class/User.class.php
+++ class/User.class.php
@@ -22,5 +22,30 @@ class User {
                                             $this->firstName, $this->lastName);
         $preparedQuery->execute();
     }
+
+    public function login() {
+        $query = "SELECT * FROM user WHERE login = ? LIMIT 1";
+        $db = new mysqli('localhost', 'root', '', 'loginForm');
+        $preparedQuery = $db->prepare($query); 
+        $preparedQuery->bind_param('s', $this->login);
+        $preparedQuery->execute();
+        $result = $preparedQuery->get_result();
+        if($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            $passwordHash = $row['password'];
+            if(password_verify($this->password, $passwordHash)) {
+                $this->id = $row['id'];
+                $this->firstName = $row['firstName'];
+                $this->lastName = $row['lastName'];
+                echo "zalogowano poprawnie!";
+            } else {
+                echo "Błędny login lub hasło!";
+            }
+        } else {
+            //no matching rows - exit the function
+            echo "Błędny login lub hasło!";
+            return;
+        }
+    }
 }
 ?>
--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 require_once('class/User.class.php');
 
-$user = new User('jkowalski', 'tajneHasło');
-$user->register();
-
+$user = new User('jkowalski1', 'tajneHasło');
+//$user->register();
+$user->login();
 echo '<pre>';
 var_dump($user);
 ?>
