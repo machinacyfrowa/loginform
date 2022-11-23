aef20d9ae354845ee19b9f7af7d479034dfa942e
Pawel Zawadzki <admin@machinacyfrowa.pl>
2022-11-23 13:19:22 +0100

    twig test1

--- config.php
+++ config.php
@@ -1,4 +1,11 @@
 <?php
+require_once('vendor/autoload.php');
+
+//loader to taki pomocnik do ładowania szablonów
+$loader = new Twig\Loader\FilesystemLoader('templates');
+//inicjujemy twiga
+$twig = new Twig\Environment($loader);
+
 $db = new mysqli('localhost', 'root', '', 'loginform');
 
 ?>
